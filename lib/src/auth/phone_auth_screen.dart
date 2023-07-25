9ecbef7bba081151537f38f822f88184c82d8f1d
abdullah <aam.msr@gmail.com>
2023-07-25 14:23:59 +0600

    added PhoneVerificationScreen

--- lib/src/auth/phone_auth_screen.dart
+++ lib/src/auth/phone_auth_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl_phone_field/intl_phone_field.dart';
+import 'package:nectar_clone/src/auth/phone_verification_screen.dart';
 import 'package:nectar_clone/src/auth/sign_in_screen.dart';
 import 'package:nectar_clone/src/utils/utils.dart';
 
@@ -8,66 +9,76 @@ class PhoneAuthScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-        leading: IconButton(
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+          leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).pushAndRemoveUntil(
+                MaterialPageRoute(
+                  builder: (context) => const SignInScreen(),
+                ),
+                (route) => false,
+              );
+            },
+            icon: const Icon(
+              Icons.chevron_left_rounded,
+              size: 32,
+              color: Color(AppColor.primaryTextColor),
+            ),
+          ),
+        ),
+        body: const Padding(
+          padding: EdgeInsets.all(25),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Enter your mobile number',
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.w500,
+                  color: Color(AppColor.primaryTextColor),
+                ),
+              ),
+              SizedBox(height: 27),
+              Text(
+                'Mobile Number',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  color: Color(AppColor.secondaryTextColor),
+                ),
+              ),
+              SizedBox(height: 10),
+              IntlPhoneField(
+                initialCountryCode: 'BD',
+                showDropdownIcon: false,
+                dropdownTextStyle: TextStyle(fontSize: 18),
+                decoration: InputDecoration(
+                  counterText: '',
+                  contentPadding: EdgeInsets.symmetric(vertical: 14),
+                ),
+                style: TextStyle(fontSize: 18),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
           onPressed: () {
             Navigator.of(context).pushAndRemoveUntil(
               MaterialPageRoute(
-                builder: (context) => const SignInScreen(),
+                builder: (context) => const PhoneVerificationScreen(),
               ),
               (route) => false,
             );
           },
-          icon: const Icon(
-            Icons.chevron_left_rounded,
-            size: 32,
-            color: Color(AppColor.primaryTextColor),
-          ),
+          backgroundColor: const Color(AppColor.primaryButtonColor),
+          child: const Icon(Icons.chevron_right_rounded, size: 32),
         ),
-      ),
-      body: const Padding(
-        padding: EdgeInsets.all(25),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Enter your mobile number',
-              style: TextStyle(
-                fontSize: 26,
-                fontWeight: FontWeight.w500,
-                color: Color(AppColor.primaryTextColor),
-              ),
-            ),
-            SizedBox(height: 27),
-            Text(
-              'Mobile Number',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w500,
-                color: Color(AppColor.secondaryTextColor),
-              ),
-            ),
-            SizedBox(height: 10),
-            IntlPhoneField(
-              initialCountryCode: 'BD',
-              showDropdownIcon: false,
-              dropdownTextStyle: TextStyle(fontSize: 18),
-              decoration: InputDecoration(
-                counterText: '',
-                contentPadding: EdgeInsets.symmetric(vertical: 14),
-              ),
-              style: TextStyle(fontSize: 18),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: const Color(AppColor.primaryButtonColor),
-        child: const Icon(Icons.chevron_right_rounded, size: 32),
       ),
     );
   }
---
+++ lib/src/auth/phone_verification_screen.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:nectar_clone/src/auth/phone_auth_screen.dart';
+import 'package:nectar_clone/src/utils/utils.dart';
+
+class PhoneVerificationScreen extends StatefulWidget {
+  const PhoneVerificationScreen({super.key});
+
+  @override
+  State<PhoneVerificationScreen> createState() =>
+      _PhoneVerificationScreenState();
+}
+
+class _PhoneVerificationScreenState extends State<PhoneVerificationScreen> {
+  final codeController = TextEditingController();
+
+  @override
+  void dispose() {
+    codeController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+          leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).pushAndRemoveUntil(
+                MaterialPageRoute(
+                  builder: (context) => const PhoneAuthScreen(),
+                ),
+                (route) => false,
+              );
+            },
+            icon: const Icon(
+              Icons.chevron_left_rounded,
+              size: 32,
+              color: Color(AppColor.primaryTextColor),
+            ),
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(25),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Enter your 4-digit code',
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.w500,
+                  color: Color(AppColor.primaryTextColor),
+                ),
+              ),
+              const SizedBox(height: 27),
+              const Text(
+                'Code',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  color: Color(AppColor.secondaryTextColor),
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextFormField(
+                controller: codeController,
+                keyboardType: TextInputType.number,
+                inputFormatters: [
+                  LengthLimitingTextInputFormatter(4),
+                ],
+                style: const TextStyle(fontSize: 18),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: Padding(
+          padding: const EdgeInsets.only(left: 25),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              TextButton(
+                onPressed: () {},
+                child: const Text(
+                  'Resend Code',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                    color: Color(AppColor.primaryButtonColor),
+                  ),
+                ),
+              ),
+              FloatingActionButton(
+                onPressed: () {},
+                backgroundColor: const Color(AppColor.primaryButtonColor),
+                child: const Icon(Icons.chevron_right_rounded, size: 32),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
