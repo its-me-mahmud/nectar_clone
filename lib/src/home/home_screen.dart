dcc710e741c69441f087d276f323aec0207be182
abdullah <aam.msr@gmail.com>
2023-08-06 18:47:39 +0600

    updated HomeScreen

--- lib/src/home/home_screen.dart
+++ lib/src/home/home_screen.dart
@@ -2,187 +2,76 @@ import 'package:flutter/material.dart';
 import 'package:nectar_clone/src/home/widgets/widgets.dart';
 import 'package:nectar_clone/src/utils/utils.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+  final _screens = [const ProductList()];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                child: Column(
-                  children: [
-                    const SizedBox(height: 25),
-                    Image.asset('assets/icon/icon.png', height: 28),
-                    const SizedBox(height: 8),
-                    const Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.location_pin,
-                          color: Color(AppColor.secondaryTextColor),
-                        ),
-                        SizedBox(width: 8),
-                        Text(
-                          'Dhaka, Banasree',
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w500,
-                            color: Color(AppColor.secondaryTextColor),
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 20),
-                    const SearchBar(
-                      elevation: MaterialStatePropertyAll(0),
-                      backgroundColor:
-                          MaterialStatePropertyAll(Color(0xfff2f3f2)),
-                      leading: Icon(
-                        Icons.search_rounded,
-                        color: Color(AppColor.secondaryTextColor),
-                      ),
-                      hintText: 'Search Store',
-                      hintStyle: MaterialStatePropertyAll(
-                        TextStyle(
-                          color: Color(AppColor.secondaryTextColor),
-                        ),
-                      ),
-                      shape: MaterialStatePropertyAll(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(15)),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    Image.asset('assets/image/banner.png'),
-                    const SizedBox(height: 30),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                child: ReusableHeadline('Exclusive Offer', onTap: () {}),
-              ),
-              const SizedBox(height: 20),
-              const Padding(
-                padding: EdgeInsets.only(left: 25),
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  padding: EdgeInsets.only(right: 25),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      ReusableProductItem(
-                        imageUrl: 'fruit/banana',
-                        title: 'Organic Bananas',
-                        subtitle: '7pcs',
-                        price: 4.99,
-                      ),
-                      SizedBox(width: 8),
-                      ReusableProductItem(
-                        imageUrl: 'fruit/apple',
-                        title: 'Red Apple',
-                        subtitle: '1kg',
-                        price: 5.49,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              const SizedBox(height: 30),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                child: ReusableHeadline('Best Selling', onTap: () {}),
-              ),
-              const SizedBox(height: 20),
-              const Padding(
-                padding: EdgeInsets.only(left: 25),
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  padding: EdgeInsets.only(right: 25),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      ReusableProductItem(
-                        imageUrl: 'vegetable/pepper',
-                        title: 'Bell Pepper Red',
-                        subtitle: '1kg',
-                        price: 5.55,
-                      ),
-                      SizedBox(width: 8),
-                      ReusableProductItem(
-                        imageUrl: 'vegetable/ginger',
-                        title: 'Ginger',
-                        subtitle: '250gm',
-                        price: 2.99,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              const SizedBox(height: 30),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                child: ReusableHeadline('Groceries', onTap: () {}),
-              ),
-              const SizedBox(height: 20),
-              const Padding(
-                padding: EdgeInsets.only(left: 25),
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  padding: EdgeInsets.only(right: 25),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      ReusableProductCategory(
-                        'Pulses',
-                        imageUrl: 'grocery/pulses',
-                        color: 0xfff8a44c,
-                      ),
-                      SizedBox(width: 10),
-                      ReusableProductCategory(
-                        'Rice',
-                        imageUrl: 'grocery/rice',
-                        color: AppColor.primaryColor,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20),
-              const Padding(
-                padding: EdgeInsets.only(left: 25),
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  padding: EdgeInsets.only(right: 25),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      ReusableProductItem(
-                        imageUrl: 'meat/beef',
-                        title: 'Beef Bone',
-                        subtitle: '1kg',
-                        price: 15.75,
-                      ),
-                      SizedBox(width: 8),
-                      ReusableProductItem(
-                        imageUrl: 'meat/chicken',
-                        title: 'Broiler Chicken',
-                        subtitle: '1kg',
-                        price: 4.50,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              const SizedBox(height: 25),
-            ],
+      body: IndexedStack(index: _currentIndex, children: _screens),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/icon/shop.png',
+              color: _currentIndex == 0
+                  ? const Color(AppColor.primaryColor)
+                  : null,
+            ),
+            label: 'Shop',
           ),
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/icon/explore.png',
+              color: _currentIndex == 1
+                  ? const Color(AppColor.primaryColor)
+                  : null,
+            ),
+            label: 'Explore',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/icon/cart.png',
+              color: _currentIndex == 2
+                  ? const Color(AppColor.primaryColor)
+                  : null,
+            ),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/icon/favourite.png',
+              color: _currentIndex == 3
+                  ? const Color(AppColor.primaryColor)
+                  : null,
+            ),
+            label: 'Favourite',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/icon/account.png',
+              color: _currentIndex == 4
+                  ? const Color(AppColor.primaryColor)
+                  : null,
+            ),
+            label: 'Account',
+          ),
+        ],
+        onTap: (value) => setState(() => _currentIndex = value),
+        currentIndex: _currentIndex,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: const Color(AppColor.primaryColor),
+        selectedLabelStyle: const TextStyle(
+          fontSize: 12,
+          fontWeight: FontWeight.w500,
         ),
       ),
     );
--- lib/src/home/widgets/widgets.dart
+++ lib/src/home/widgets/widgets.dart
@@ -1,3 +1,4 @@
+export 'product_list.dart';
 export 'reusable_headline.dart';
 export 'reusable_product_category.dart';
 export 'reusable_product_item.dart';
